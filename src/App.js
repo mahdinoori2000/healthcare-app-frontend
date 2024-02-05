714a7503ae6d392357209e03cc66d717dd785d61
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-05 10:03:08 +0430

    Add font family

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import DoctorDetails from './pages/DoctorDetails';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Navbar />
       <Routes>
         <Route path="/" element={<Doctors />} />
