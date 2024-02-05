5b89f0c0c037fd14b2f9a391fecbad6c4596b5ac
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-05 09:49:42 +0430

    Add doctor details route.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MyAppointments from './pages/MyAppointments';
 import AddDoctor from './pages/AddDoctor';
 import BookAppointment from './pages/BookAppointment';
 import DeleteDoctor from './pages/DeleteDoctor';
+import DoctorDetails from './pages/DoctorDetails';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Doctors />} />
+        <Route path="/doctors/:id" element={<DoctorDetails />} />
         <Route path="/book-appointment" element={<BookAppointment />} />
         <Route path="/my-appointments" element={<MyAppointments />} />
         <Route path="/add-doctor" element={<AddDoctor />} />
