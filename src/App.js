87c2c2d1ade806ecbcebaa36d18667c84150bc72
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-05 09:46:11 +0430

    Add routes to the project.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Doctors from './pages/Doctors';
+import Navbar from './componenets/navbar/navbar/Navbar';
+import MyAppointments from './pages/MyAppointments';
+import AddDoctor from './pages/AddDoctor';
+import BookAppointment from './pages/BookAppointment';
+import DeleteDoctor from './pages/DeleteDoctor';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is the application</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Doctors />} />
+        <Route path="/book-appointment" element={<BookAppointment />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/add-doctor" element={<AddDoctor />} />
+        <Route path="/delete-doctor" element={<DeleteDoctor />} />
+      </Routes>
     </div>
   );
 }
