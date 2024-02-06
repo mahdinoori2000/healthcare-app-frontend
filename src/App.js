3fe0a676a250e31327b4306d3eb482dfd7ace365
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-06 17:24:46 +0430

    Add protected routes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Doctors from './pages/Doctors';
 import Navbar from './components/navbar/Navbar';
 import MyAppointments from './pages/MyAppointments';
 import AddDoctor from './pages/AddDoctor';
 import BookAppointment from './pages/BookAppointment';
 import DeleteDoctor from './pages/DeleteDoctor';
 import DoctorDetails from './pages/DoctorDetails';
+import SplashScreen from './components/splashScreen/SplashScreen';
+import Login from './components/login/login';
+import Register from './components/register/Register';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Doctors />} />
-        <Route path="/doctors/:id" element={<DoctorDetails />} />
-        <Route path="/book-appointment" element={<BookAppointment />} />
-        <Route path="/my-appointments" element={<MyAppointments />} />
-        <Route path="/add-doctor" element={<AddDoctor />} />
-        <Route path="/delete-doctor" element={<DeleteDoctor />} />
+        <Route exact path="/" element={<SplashScreen />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/doctors/:id" element={<DoctorDetails />} />
+          <Route path="/book-appointment" element={<BookAppointment />} />
+          <Route path="/my-appointments" element={<MyAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/delete-doctor" element={<DeleteDoctor />} />
+        </Route>
       </Routes>
     </div>
   );
