48207ae80ce64d8e0ac6102b1f2f49dd971b4604
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-08 00:21:41 +0430

    Add design for login signup

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Cookies from 'js-cookie';
 import { logout } from '../../redux/user/userSlice';
-import './navbar.css';
+import logo from '../../assets/logo.png';
+import navbar from './navbar.module.css';
 
 const Navbar = () => {
   const { userData } = useSelector((store) => store.user);
+  const location = useLocation();
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -16,24 +18,40 @@ const Navbar = () => {
     navigate('/');
   };
   return (
-
-    <nav className="nav-container">
-      <div className="profile">
-        <p>{`Welcome ${userData.user.name}`}</p>
+    <nav className={navbar.nav_container}>
+      <div className={navbar.profile}>
+        <img src={logo} alt="Logo" className={navbar.logo} />
+        <p>{`Welcome "${userData.user.name}"`}</p>
       </div>
-      <div className="nav-links">
-        <NavLink to="/doctors">Doctors</NavLink>
-        <NavLink to="/doctors/book-appointment">Book Appointment</NavLink>
-        <NavLink to="/doctors/my-appointments">My Appointments</NavLink>
-        <NavLink to="/doctors/add-doctor">Add Doctor</NavLink>
-        <NavLink to="/doctors/delete-doctor">Delete Doctor</NavLink>
+      <div className={navbar.nav_links}>
+        <NavLink to="/doctors" className={location.pathname === '/doctors' ? navbar.active : ''}>Doctors</NavLink>
+        <NavLink to="/doctors/book-appointment" className={location.pathname === '/doctors/book-appointment' ? navbar.active : ''}>Book Appointment</NavLink>
+        <NavLink to="/doctors/my-appointments" className={location.pathname === '/doctors/my-appointments' ? navbar.active : ''}>My Appointments</NavLink>
+        <NavLink to="/doctors/add-doctor" className={location.pathname === '/doctors/add-doctor' ? navbar.active : ''}>Add Doctor</NavLink>
+        <NavLink to="/doctors/delete-doctor" className={location.pathname === '/doctors/delete-doctor' ? navbar.active : ''}>Delete Doctor</NavLink>
       </div>
       <button
+        className={navbar.logout}
         onClick={logoutUser}
         type="button"
       >
         Logout
       </button>
+      <ul className={navbar.social_links}>
+        <li>
+          <img src="https://jade-rabanadas-479b96.netlify.app/static/media/fb.6ede2e0d2f244fec1b76327b30e2d180.svg" alt="" />
+        </li>
+        <li>
+          <img src="https://jade-rabanadas-479b96.netlify.app/static/media/x.068f9efee7b52e070424b4c2925f2960.svg" alt="" />
+        </li>
+        <li>
+          <img src="https://jade-rabanadas-479b96.netlify.app/static/media/google.cf737c214060d873db8410bbd35aa463.svg" alt="" />
+        </li>
+        <li>
+          <img src="https://jade-rabanadas-479b96.netlify.app/static/media/linkedin.6bfb62956b5c794df40ab6045a56d21d.svg" alt="" />
+        </li>
+      </ul>
+      <p className={navbar.copyright}>@2024 Health Care App</p>
     </nav>
   );
 };
