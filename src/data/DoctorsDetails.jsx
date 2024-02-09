adbd1ea901b136bc9458220f737534f2d38c4665
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-09 11:06:28 +0430

    Add navbar mock data.

---
+++ src/data/DoctorsDetails.jsx
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+  {
+    id: 0,
+    name: 'Farhad Mominzada',
+    bio:
+        'Dedicated to compassionate care and patient well-being.',
+    image_url: 'https://placekitten.com/800/400',
+    specialization: 'Dentist',
+  },
+];
