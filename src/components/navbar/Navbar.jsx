6f0bf84d9c2f42b547380fb6009fe75118811dac
Mahdi Noori <mahdinoori2389@gmail.com>
2024-02-14 22:55:02 +0430

    Install prop-types

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,8 +18,6 @@ export default function Navbar() {
   }, []);
 
   return (
-    <>
-      {mobileScreenSize ? <MobileNav /> : <DesktopNav />}
-    </>
+    mobileScreenSize ? <MobileNav /> : <DesktopNav />
   );
 }
