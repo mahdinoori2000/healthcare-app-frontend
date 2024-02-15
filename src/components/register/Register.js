c8e820c4e4910ab74821d1addcb3e600aa4539e5
Ismail mayito <ismayito1@gmail.com>
2024-02-15 21:10:56 +0300

    Fix typo in the sign up page

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -121,7 +121,7 @@ function Register() {
                     className="bg-green-primary-color mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
                     type="submit"
                   >
-                    Log in
+                    Sign up
                   </button>
                 </div>
 
@@ -132,7 +132,7 @@ function Register() {
                       type="button"
                       className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                     >
-                      Sign up
+                      Login
                     </button>
                   </NavLink>
                 </div>
